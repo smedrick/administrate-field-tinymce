635153f8b6d20030593d77f3f1129d14940b1a5a
Richard Watkins <smedrick@gmail.com>
2023-08-12 16:35:01 -0400

    Update application.js

--- app/assets/javascripts/administrate-field-tinymce/application.js
+++ app/assets/javascripts/administrate-field-tinymce/application.js
@@ -2,7 +2,7 @@
 //= require tinymce-jquery
 
 tinyMCE.init({
-  selector: 'textarea#tinymce',
+  selector: 'textarea',
   plugins : 'advlist autolink link image lists charmap preview code',
   relative_urls: false,
   remove_script_host : false,
